247828cc4ac8fafef3652acfaff6b46565ec3bb3
Hasham M Khan <hasham.m.khan@outlook.com>
2025-05-29 23:10:07 -0500

    Adds controller for the Book class mapping

---
+++ src/main/java/com/springframework/webapp/controllers/BookController.java
@@ -0,0 +1,23 @@
+package com.springframework.webapp.controllers;
+
+import com.springframework.webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepo;
+
+    public BookController(final BookRepository bookRepo) {
+        this.bookRepo = bookRepo;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepo.findAll());
+
+        return "books";
+    }
+}
